fde9f269d8d34cd1ed3ad3cfab4ab56dcbde4387
oneSunBody <1902866216@qq.com>
2018-12-04 16:27:53 +0800

    master分支代码

--- item/skk.js
+++ item/skk.js
@@ -1,1 +1,8 @@
-
+document.querySelector("header").addEventListener("click", function (e) {
+    if (e.target.tagName == "SPAN") {
+        document.querySelectorAll("header span").forEach((el) => {
+            el.classList.remove("cur")
+        })
+        e.target.classList.add("cur")
+    }
+})
